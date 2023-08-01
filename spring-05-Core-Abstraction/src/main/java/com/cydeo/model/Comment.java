0081b16c01552e6e96475cfe7c2c5b707d26962f
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-01 14:09:16 +0300

    added a comment to entity class.

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 //We did not put here @Component annotation because this class doesn't have a class instance variable inside (doesn't have a has a relationship) and in our application any other class has a relationship with this class means any other class has it as a instance variable.(any other class doesn't have a has a relationship with this class) WE DON'T ADD @COMPONENT TO ENTITY/MODEL CLASSES.
+//We did not create any methods at entity classes. Only the instance variables (properties of that class).
 public class Comment {
     private String author;
     private String text;
