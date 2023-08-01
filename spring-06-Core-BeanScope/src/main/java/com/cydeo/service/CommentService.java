790d2a3ec1f35558c954105e010cda052b2c157b
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-01 14:21:37 +0300

    we add the @Scope annotation.

--- spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -4,9 +4,13 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)//By Scope annotation we say spring to not use singleton but prototype means create new beans for all the request of this class. Otherwise as a default it use singleton and give the same object/bean all the time.
 public class CommentService {
 
     private final CommentRepository commentRepository;
