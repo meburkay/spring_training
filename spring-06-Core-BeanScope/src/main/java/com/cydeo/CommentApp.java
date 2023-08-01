b18d310e9c92a286840d62e37ec285536082412a
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-01 23:38:50 +0300

    I add some comments

--- spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -20,9 +20,10 @@ public class CommentApp {
         CommentService cs1 = context.getBean(CommentService.class);
         CommentService cs2 = context.getBean(CommentService.class);
 
-        System.out.println(cs1);
-        System.out.println(cs2);
-        System.out.println(cs1==cs2);
+        //We change the scope of spring at the CommentService to prototype and because of that spring give us new beans when we request. Below we test it.
+        System.out.println(cs1);        //com.cydeo.service.CommentService@408d971b
+        System.out.println(cs2);        //com.cydeo.service.CommentService@6c6cb480
+        System.out.println(cs1==cs2);   //false
 
     }
 }
