01e8e8002f54983e65579a7eff4dfdfc4f2fb558
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-03 10:53:02 +0300

    SpringBoot we add Core-Abstraction files and use SpringBoot to execute.

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,28 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)//By Scope annotation we say spring to not use singleton but prototype means create new beans for all the request of this class. Otherwise as a default it use singleton and give the same object/bean all the time.
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
