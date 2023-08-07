5fa4b7a292c39958b63352593f0684dc01f91cff
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-07 19:20:51 +0300

    some review notes

--- spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
+++ spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
@@ -10,9 +10,16 @@ import org.springframework.context.annotation.Configuration;
 public class PcConfig {
 
     @Bean
-    public Case caseDell(Dimensions dimensions){//We give Dimensions parameter and spring automatically give here the Dimensions bean. It is autowiring. If we have another bean that need Dimensions spring use the same Dimensions bean object and autowire it to that bean too. It does not create a new object/bean for it. It is the advantage of spring framework.
+    public Case caseDell(Dimensions dimensions){//We give Dimensions parameter and spring automatically give here the Dimensions bean. If we have another bean that need Dimensions spring use the same Dimensions bean object and wire it to that bean too. It does not create a new object/bean for it. It is the advantage of spring framework.
 
         return new DellCase("220B","Dell","240",dimensions);
+    }
+
+
+    @Bean
+    public Case caseDell(){//This is the second way to wire. We directly call the bean method inside the method.
+
+        return new DellCase("220B","Dell","240",dimensions());
     }
     @Bean
     public Dimensions dimensions(){
