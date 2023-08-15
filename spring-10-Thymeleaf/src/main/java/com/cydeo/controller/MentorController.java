b05765584d7c36537acc09e59d442dbb72eb3d83
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-15 12:20:34 +0300

    form fields completed

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
@@ -13,6 +16,11 @@ public class MentorController {
     @GetMapping("/register") //localhost:8080/mentor/register
     public String register(Model model){
 
+        //We create this list for the dropdown button at the view. Then we loop it there and use.
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+
+        //We create an empty Mentor object for the form to fill it.
         model.addAttribute("mentor",new Mentor());
 
         return "mentor/mentor-register";
