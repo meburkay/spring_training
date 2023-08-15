c48b82103fc31e1fcfc0fc397636986757106225
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-15 11:09:39 +0300

    @GetMapping @PostMapping

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,16 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")//When we use this it will add all the method end points /student
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/student/register
+    //@RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/student/register
+    @GetMapping("/register")//get mapping to request a view to see.
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -20,7 +18,8 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome") //localhost:8080/student/welcome
+    @RequestMapping(value = "/welcome",method = RequestMethod.POST) //localhost:8080/student/welcome?name=Ozzy
+    @PostMapping("/welcome")//to send a data to server
     public String welcome(@RequestParam String name){//We add link a parameter with parenthesis and capture it here as parameter.
 
 
