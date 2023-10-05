c96433672f0257b1f32c175734a25604d2cb2f04
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-05 22:29:56 +0300

    table creating SQL

--- spring-11-Data-TableCreation/src/main/resources/schema.sql
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+-- We add this line because every time we build our app the same table tried to create and give exception.
+drop table IF exists EMPLOYEES;
+
 create table EMPLOYEES(
 
                           id bigint not null,
