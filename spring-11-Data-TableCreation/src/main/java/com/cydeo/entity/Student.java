a4890f6a37efb2117578dcce9d3516a0b82520f9
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 12:15:07 +0300

    used new annotations @Table, @GeneratedValue, @Column, @Enumerated

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,34 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")//Normally when you use only @Entity annotation Spring create a table with "student" name. But you can change that with @Table annotation. To do that and give the table name plural is the best practice. Because it is a table that consist of students.
 public class Student {
 
-    @Id
+    @Id//means primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//means that spring you take the primary key responsibility.
     private Long id;
+
+    @Column(name = "studentFirstName")//change the column name
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
 
+    @Column(columnDefinition = "DATE")//seting the column type
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime birthDateTime;
+
+    @Enumerated(EnumType.STRING)//For enums we use this annotation to change it to String. Otherwise it is understanded as integer as a default.
+//    @Enumerated(EnumType.ORDINAL) // this is the default form.
+    private Gender gender;
 }
