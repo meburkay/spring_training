d302d6900980d9d2b3dfbbfb27821969db0cf047
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 20:40:53 +0300

    datagenerator, paymentrepository and cascade configuration added

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,47 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.entity.Payment;
+import com.cydeo.entity.PaymentDetail;
+import com.cydeo.enums.Status;
+import com.cydeo.repository.PaymentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    private final PaymentRepository paymentRepository;
+
+    public DataGenerator(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Payment payment1 = new Payment(LocalDate.of(2022,4,19),new BigDecimal("150000"), Status.SUCCESS);
+        PaymentDetail paymentDetail1 = new PaymentDetail(new BigDecimal("140000"),new BigDecimal("10000"),LocalDate.of(2022,4,24));
+
+        payment1.setPaymentDetail(paymentDetail1);
+
+        Payment payment2 = new Payment(LocalDate.of(2022,4,25),new BigDecimal("100000"), Status.FAILURE);
+        PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),LocalDate.of(2022,4,29));
+
+        payment2.setPaymentDetail(paymentDetail2);
+
+        //Because of the cascade at the Payment's PaymentDetail instance when we save payment1 to Payment table it automatically save PaymentDetail1 to PaymentDetail table also.
+        //If we do not use cascade we must create PaymentDetailRepository, and we have to save PaymentDetail data to tables then we must execute below code. Otherwise, it gives exception.
+        paymentRepository.save(payment1);
+        paymentRepository.save(payment2);
+
+        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
+
+        paymentRepository.delete(payment1);
+
+
+
+    }
+}
--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -26,8 +26,10 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private Status paymentStatus;
 
-    @OneToOne//this annotation creates the join relationship and by this when the table payment table created spring automatically add to it the PaymentDetail foregin key.
+    //@OneToOne//this annotation creates the join relationship and by this when the table payment table created spring automatically add to it the PaymentDetail foregin key.
     //@JoinColumn(name = "payment_detail_id")//with this we can change the foreign key name. In default it is payment_detail_id and generally people use the default way.
+    //@OneToOne(cascade = CascadeType.ALL)//with cascade we say that what happens to Payment as a parent it happens to PaymentDetail too. For now because of that with only PaymentRepository we can do our operations.
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})//by this way we can give whatever type we wanna give.
     private PaymentDetail paymentDetail;
 
 
