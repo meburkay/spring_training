30eedbd53fc7b7fa50340ec2d07dbaca4eaf535d
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 14:10:23 +0300

    @Repository annotation

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -3,7 +3,9 @@ package com.cydeo.repository;
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository//We do not have to put it anymore.
 public interface CarRepository extends JpaRepository<Car,Long> {
 
 
