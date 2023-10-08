3f4f8cbcc7e40b33b9288a71aa8fe06589b8d06b
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 18:37:05 +0300

    some errors corrected

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -1,10 +1,10 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
