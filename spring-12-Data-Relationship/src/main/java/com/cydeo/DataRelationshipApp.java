3a9f01a7e18ad58bf6a31567390b4892def477d6
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 18:21:49 +0300

    some errors corrected

--- spring-12-Data-Relationship/src/main/java/com/cydeo/DataRelationshipApp.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/DataRelationshipApp.java
@@ -1,13 +1,13 @@
-package com.cydeo.spring12datarelationship;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring12DataRelationshipApplication {
+public class DataRelationshipApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring12DataRelationshipApplication.class, args);
+		SpringApplication.run(DataRelationshipApp.class, args);
 	}
 
 }
