d9df2cd94793deca6cfffe73aca4cd27b27ecead
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-08 14:21:08 +0300

    general practice classes and repositories created

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,15 +1,26 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-@Entity//This annotation means that this class will be a table at the database.
-public class Employee {
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-    @Id//Primary key annotation
-    private int id;
-    private  String name;
+import javax.persistence.*;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends BaseEntity {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
 
 }
