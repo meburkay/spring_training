c3f0cdeef954a87e96e76cb88a19ec643e2888b4
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-13 12:33:31 +0300

    REST-Jackson annotations examples

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -24,6 +24,7 @@ public class AccountDTO {
     private String postalCode;
     //In general this annotation used with @JsonManagedReference annotation. We use this annotations when a collision happens between two classes like here. Here we have user and account dto and they have each others field at the same time. Because of that we use this annotations on them to solve the issue. BackReference annotation field will not serialized (When we retrieve account data from the api we won't see the user information) and the other feild which is account belongs to user will be serialized. By this we solve the collision issue here.
     @JsonBackReference  // This field is not going to be serialized
+    //@JsonIgnore The difference between this and @JsonBackReference is when we use @jsonIgnore we can't get or post anything about that feild. But when we use @JsonBackReference we can post data about it.
     private UserDTO user;
 
 }
