28786e1d068d2cae721ad44a3f145d2a49eea1aa
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-13 23:42:20 +0300

    spring-18-REST-SchoolLab jackson annotations

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
@@ -1,6 +1,7 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,10 +23,15 @@ public class AddressDTO {
 
     private AddressType addressType;
 
+
+    //If we use more than one @JaonBackReference in a DTO we must give value to them to let spring know which one connects to which one. We give the same value to the @JsonManagedReference too.
+    @JsonBackReference(value = "student-address-reference")          // defaultReference
     private StudentDTO student;
 
+    @JsonBackReference(value = "parent-address-reference")          // defaultReference
     private ParentDTO parent;
 
+    @JsonBackReference(value = "teacher-address-reference")          // defaultReference
     private TeacherDTO teacher;
 
     private Integer currentTemperature;
