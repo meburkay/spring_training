c8dc0df0003b49c40db31ee0e4260bd445162ff7
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-13 12:28:45 +0300

    REST-ConsumingApis WebClient added and for that mvc structure and repisatory/services added

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 @EnableFeignClients//For consuming apis by feign way we must put this here.
@@ -20,5 +21,12 @@ public class Spring17RestConsumingApisApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
 
 }
