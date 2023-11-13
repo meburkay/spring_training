abfbcceba8299b7558e1a8eaf94fc6ed811066a8
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-13 11:57:32 +0300

    REST-Jackson annotation examples

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,20 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//We can give list like below for @JsonIgnore
+//@JsonIgnoreProperties(value = {"address", "country"}, ignoreUnknown = true)
+//ignoreUnknown means ignore the data that is unknown. Normally it ignores but as a best practice we must use like this for a second security about the unknown files.
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDTO {
 
+    @JsonIgnore//Ignores the field when consuming or sending in both ways.
     private String name;
     private String address;
     private String country;
@@ -14,6 +22,8 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+    //In general this annotation used with @JsonManagedReference annotation. We use this annotations when a collision happens between two classes like here. Here we have user and account dto and they have each others field at the same time. Because of that we use this annotations on them to solve the issue. BackReference annotation field will not serialized (When we retrieve account data from the api we won't see the user information) and the other feild which is account belongs to user will be serialized. By this we solve the collision issue here.
+    @JsonBackReference  // This field is not going to be serialized
     private UserDTO user;
 
 }
--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,17 +1,25 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private String email;
+
+    //When we do not want to show the information about a field we use this. By this annotation the use can change the field but can not see the information about it.
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String username;
     private UserRole role;
+    @JsonManagedReference  // This field is going to be serialized and will be showed at the json body.
     private AccountDTO account;
 
 }
