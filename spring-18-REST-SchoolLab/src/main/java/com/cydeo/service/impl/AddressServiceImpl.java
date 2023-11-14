9a41ba633aa9d431e6b7310253ea9430ba5c26b5
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-15 00:19:19 +0300

    spring-18-REST-SchoolLab Weather API consuming and lab done

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -1,10 +1,13 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.client.WeatherApiClient;
 import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.WeatherDTO;
 import com.cydeo.entity.Address;
 import com.cydeo.util.MapperUtil;
 import com.cydeo.repository.AddressRepository;
 import com.cydeo.service.AddressService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,10 +19,16 @@ public class AddressServiceImpl implements AddressService {
 
     private final AddressRepository addressRepository;
     private final MapperUtil mapperUtil;
+    private final WeatherApiClient weatherApiClient;
+    //We take the access key from application.properties file by @Value annotation.
+    @Value("${access_key}")
+    private String access_key;
 
-    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil) {
+
+    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil, WeatherApiClient weatherApiClient) {
         this.addressRepository = addressRepository;
         this.mapperUtil = mapperUtil;
+        this.weatherApiClient = weatherApiClient;
     }
 
     @Override
@@ -34,7 +43,13 @@ public class AddressServiceImpl implements AddressService {
     public AddressDTO findById(Long id) throws Exception {
         Address foundAddress = addressRepository.findById(id)
                 .orElseThrow(() -> new Exception("No Address Found!"));
-        return mapperUtil.convert(foundAddress, new AddressDTO());
+
+        AddressDTO addressDTO = mapperUtil.convert(foundAddress, new AddressDTO());
+        //To take the temperature from the api we use this line. We create a private method to take the data from api and then we retrieve the temperature from it and set it to addressDTO.
+        //We create feignclient for consuming api. And we use DTOs with it. And when we will consume api we did that operation in the service implementations class as a general convention.
+        addressDTO.setCurrentTemperature(getCurrentWeather(addressDTO.getCity()).getCurrent().getTemperature());
+
+        return addressDTO;
     }
 
     @Override
@@ -47,7 +62,10 @@ public class AddressServiceImpl implements AddressService {
 
         addressRepository.save(addressToSave);
 
-        return mapperUtil.convert(addressToSave, new AddressDTO());
+        AddressDTO updatedAddress = mapperUtil.convert(addressToSave, new AddressDTO());
+        updatedAddress.setCurrentTemperature(getCurrentWeather(updatedAddress.getCity()).getCurrent().getTemperature());
+
+        return updatedAddress;
 
     }
 
@@ -68,4 +86,7 @@ public class AddressServiceImpl implements AddressService {
 
     }
 
+    private WeatherDTO getCurrentWeather(String city) {
+        return weatherApiClient.getCurrentWeather(access_key, city);
+    }
 }
