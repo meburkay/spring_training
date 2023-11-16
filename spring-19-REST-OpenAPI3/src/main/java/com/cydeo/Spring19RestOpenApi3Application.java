45a8f354f616dfd724a892971a667c3ac5833942
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-11-16 13:49:39 +0300

    spring-19-REST-OpenAPI3 added annotations belongs to openapi dependency

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
@@ -1,9 +1,14 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Spring19RestOpenApi3Application {
@@ -15,6 +20,17 @@ public class Spring19RestOpenApi3Application {
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+
+    //By creating a bean from OpenAPI belongs to the dependency we add we can define the properties of the swagger documentation like this.
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                        .title("Cydeo Application OpenAPI")
+                        .version("v1")
+                        .description("Cydeo application API documentation"))
+                .servers(List.of(new Server().url("https://dev.cydeo.com").description("Dev Environment")));
     }
 
 }
--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
@@ -2,6 +2,9 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.AccountDTO;
 import com.cydeo.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
+//We create this annotations for openapi documentation. We can give the tag or tags like this. Extra if we want we can give this tag one by one above the methods.
+@Tag(name = "Account", description = "Account CRUD Operations")
+@Tags(value = {@Tag(name = "Account", description = "Account CRUD Operations"),
+        @Tag(name = "Account2", description = "Account2 CRUD Operations")})
 public class AccountController {
 
     private final AccountService accountService;
@@ -21,11 +28,14 @@ public class AccountController {
     }
 
     @GetMapping("/list")
+    //We create this annotation for openapi documentation to give extra information.
+    @Operation(summary = "Read all accounts")
     public ResponseEntity<List<AccountDTO>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAccounts());
     }
 
     @GetMapping
+    @Operation(summary = "Read an account by id")
     public ResponseEntity<AccountDTO> getById(@RequestParam("id") Long id) throws Exception {
         return ResponseEntity.ok(accountService.getById(id));
     }
